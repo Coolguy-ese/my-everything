7880b7f1cc4d870b78051abc0f82687c50e00a7e
Coolguy-ese <111687860+Coolguy-ese@users.noreply.github.com>
2022-08-21 15:29:40 +0100

    Add files via upload
    This website is mainly for forex trading!!!!!

---
+++ global forex/jav.js
@@ -0,0 +1,16 @@
+let me='Welcome to Global Forex Trade';
+alert(me);
+
+
+let msgname=prompt('Enter your Name ');
+        if(msgname=="Ese"){
+            alert("Welcome BOSS!!!");
+        }
+        else if(msgname==" "){
+            alert('Imput please');
+        
+        }
+        else{
+            alert('Welcome'+' '+msgname+' '+'to Global Forex Trade');
+        }
+console.log(msgname+' '+'Enter this page');
